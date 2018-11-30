3bc69577d3427cf0981771cf1f55d8165672da84
Florian-H-Lab <f.heyl@gmx.de>
2018-11-30 15:10:09 +0100

    Changes to config/cluster.yml Snakemake file and snakemake_qsub.sh

--- snakemake_qsub.sh
+++ snakemake_qsub.sh
@@ -11,4 +11,4 @@
 MINICONDA="/scratch/bi01/heylf/miniconda3/bin/:$PATH"
 export PATH=$MINICONDA
 source activate snakemake
-snakemake --use-conda -j 20 --directory ${PWD} --cluster-config cluster.yml --cluster "qsub -v PATH=$MINICONDA -R y -N {cluster.jobname} -cwd -pe {cluster.cores} -l {cluster.mem} -o {cluster.out} -e {cluster.err} -M heylf@informatik.uni-freiburg.de -m a" --latency-wait 60
+snakemake -s Snakefile_PARCLIP_singleend.py --use-conda -j 20 --directory ${PWD} --cluster-config cluster.yml --cluster "qsub -v PATH=$MINICONDA -R y -N {cluster.jobname} -cwd -pe {cluster.cores} -l {cluster.mem} -o {cluster.out} -e {cluster.err} -M heylf@informatik.uni-freiburg.de -m a" --latency-wait 60
