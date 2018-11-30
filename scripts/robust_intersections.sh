9a8ebf04df754d3c0187710799f742b724864fcc
Florian-H-Lab <f.heyl@gmx.de>
2018-11-30 15:13:29 +0100

    Adding envs/piranha.yml  scripts/robust_intersections.sh

---
+++ scripts/robust_intersections.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+INPUT_FOLDER=$1"/"
+OUTPUT_FOLDER=$2"/"
+INPUT_FORMAT=$3
+
+numfiles=$((-1))
+
+# get all files in input folder 
+declare -a FILELIST
+for f in $INPUT_FOLDER*.$INPUT_FORMAT; do 
+	numfiles=$(($numfiles+1))
+    FILELIST=("${FILELIST[@]}" "$f")
+done
+
+# pairwise intersections
+for i in `seq 0 $numfiles`; do
+    for j in `seq $(($i+1)) $numfiles`; do
+    	a=${FILELIST[$i]}
+    	a=${a//.$INPUT_FORMAT/}
+    	a=${a//$INPUT_FOLDER/}
+    	b=${FILELIST[$j]}
+    	b=${b//.$INPUT_FORMAT/}
+    	b=${b//$INPUT_FOLDER/}
+    	outfile=$OUTPUT_FOLDER$a"_vs_"$b".bed"
+		bedtools intersect -a ${FILELIST[$i]} -b ${FILELIST[$j]} -s -wa -u -f 0.1 > $outfile
+    done
+done
+
+# between all intersections
+outfile_name=$INPUT_FOLDER"robust_between_all"
+a=${FILELIST[0]}
+for i in `seq 1 $numfiles`; do
+	outfile=$outfile_name"_"$i".bed"
+	b=${FILELIST[$i]}
+	echo $a
+	echo $b
+	bedtools intersect -a $a -b $b -s -wa -u -f 0.1 > $outfile
+	a=$outfile
+done
+
+mv $a $INPUT_FOLDER"robust_between_all.bed"
+rm $INPUT_FOLDER*"robust_between_all_"*".bed"
