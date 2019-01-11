0705d0b6d54f96f5c62a0d03cee40f3fa47ea082
Florian-H-Lab <f.heyl@gmx.de>
2019-01-11 16:45:06 +0100

    Changes to snake qsub

--- snakemake_qsub.sh
+++ snakemake_qsub.sh
@@ -11,4 +11,9 @@
 MINICONDA="/scratch/bi01/heylf/miniconda3/bin/:$PATH"
 export PATH=$MINICONDA
 source activate snakemake5.3
-snakemake -s Snakefile.py --use-conda -j 20 --directory ${PWD} --cluster-config cluster.yml --cluster "qsub -v PATH=$MINICONDA -R y -N {cluster.jobname} -cwd -pe {cluster.cores} -l {cluster.mem} -o {cluster.out} -e {cluster.err} -M heylf@informatik.uni-freiburg.de -m a" --latency-wait 60
+if [ -e snakelog.txt ]
+then
+    rm snakelog.txt
+fi
+snakemake -s Snakefile.py -np > snakelog.txt
+snakemake -s Snakefile.py --conda-prefix /scratch/bi03/heylf/salamisnake_conda --use-conda -j 20 --directory ${PWD} --cluster-config cluster.yml --cluster "qsub -v PATH=$MINICONDA -R y -N {cluster.jobname} -cwd -pe {cluster.cores} -l {cluster.mem} -o {cluster.out} -e {cluster.err} -M heylf@informatik.uni-freiburg.de -m a" --latency-wait 60
