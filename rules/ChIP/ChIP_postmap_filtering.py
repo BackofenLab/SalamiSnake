823f87ef2cada7af85aa6a144c10d079f9369bc0
Florian-H-Lab <f.heyl@gmx.de>
2019-02-05 16:14:07 +0100

    Adding new rules for ChIP-Seq

---
+++ rules/ChIP/ChIP_postmap_filtering.py
@@ -0,0 +1,37 @@
+import random
+import math
+import os 
+
+########################
+## POST-MAP FILTERING ##
+########################
+
+# -F 0x100 do not include secondary alignments 
+rule unique_reads_fitlering:
+	input:
+		MAPPING_OUTDIR + "/{sample}_{replicate}.bam"
+	output:
+		PRE_FOR_UMI_OUTDIR + "/{sample}_{replicate}_unique_reads_fitlering.bam"
+	threads: 2
+	conda:
+		config["conda_envs"] + "/samtools.yml"
+	shell:
+		"if [ ! -d {PRE_FOR_UMI_OUTDIR} ]; then mkdir {PRE_FOR_UMI_OUTDIR}; fi"
+		"&& samtools view -h -F 0x100 {input} "
+		"| awk '$0 ~ /^@/{{ print }} ($2 == 163 || $2 == 147 || $2 == 83 || $2 == 99)&&$0!~/XS:i/{{ print }} ' "  
+		"| samtools view -bSh > {output}"
+
+rule filter_out_unlocalized_regions_for_later_genome_versions:
+	input:
+		PRE_FOR_UMI_OUTDIR + "/{sample}_{replicate}_unique_reads_fitlering.bam"
+	output:
+		PRE_FOR_UMI_OUTDIR + "/{sample}_{replicate}_got_umis_unlocalized_check.bam"
+	threads: 2
+	conda:
+		config["conda_envs"] + "/samtools.yml"
+	shell:
+		"samtools view -h {input} "
+		"""| awk -F "\t" 'BEGIN {{ OFS = FS }} {{ if ($0 ~ /^@/) {{print $0;}} else {{ if ($3 ~/^chr/) {{print $0;}} }} }}' """
+		"| samtools view -bSh > {output}"
+		"&& samtools index {output}"
+ 
